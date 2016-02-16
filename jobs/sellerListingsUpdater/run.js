62862797044f223fe99772de4d0b1f26e9615610
¨Gaston <gastonprietoutn@gmail.com>
2016-02-16 09:19:50 -0300

    Adding callbacks in runner SellerListingUpdater

--- jobs/sellerListingsUpdater/run.js
+++ jobs/sellerListingsUpdater/run.js
@@ -1,2 +1,9 @@
 require("coffee-script/register");
-require("./src/run.coffee");
+require("./src/run.coffee").process({
+  start: function (task) {
+    console.log('Start page', task.page);
+  },
+  finish: function (err, task) {
+    console.log('Finish page', task.page);
+  }
+})
